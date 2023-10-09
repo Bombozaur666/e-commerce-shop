a0289681c749397525974c8d6b5a4517e09e8453
Bombozaur666 <33629581+Bombozaur666@users.noreply.github.com>
2023-10-09 18:13:43 +0200

    Update settings.py

--- src/main/settings.py
+++ src/main/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_results',
+    'corsheaders',
 ]
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -152,3 +154,12 @@ CELERY_TRACK_STARTED = os.environ['CELERY_TRACK_STARTED']
 
 CELERY_RESULT_BACKEND = os.environ['CELERY_RESULT_BACKEND']
 CELERY_CACHE_BACKEND = os.environ['CELERY_CACHE_BACKEND']
+
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    f'{os.environ["CORS_API"]}',
+] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    f'{os.environ["CORS_API"]}',
+]
