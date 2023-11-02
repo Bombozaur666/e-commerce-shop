92c3d935f07fc82c416b23ad6cca9b6cab39f40a
Bombo <bombozaur6@gmail.com>
2023-11-02 03:26:12 +0100

    add more accounts tests

--- src/tests/tests_accounts.py
+++ src/tests/tests_accounts.py
@@ -22,6 +22,38 @@ def test_accounts(client, django_user_model):
         "last_name": user_last_name,
     }
 
+    username2 = "Alber"
+    user_data_no_password = {
+        "username": username2,
+        "email": user_email,
+        "first_name": user_first_name,
+        "last_name": user_last_name,
+    }
+    user_data_no_username = {
+        "password": user_password,
+        "email": user_email,
+        "first_name": user_first_name,
+        "last_name": user_last_name,
+    }
+    user_data_no_email = {
+        "username": username2,
+        "password": user_password,
+        "first_name": user_first_name,
+        "last_name": user_last_name,
+    }
+    user_data_no_first_name = {
+        "username": username2,
+        "password": user_password,
+        "email": user_email,
+        "last_name": user_last_name,
+    }
+    user_data_no_last_name = {
+        "username": username2,
+        "password": user_password,
+        "email": user_email,
+        "first_name": user_first_name,
+    }
+
     resp = client.post("/accounts/create/", user_data)
     assert resp.status_code == 201
     assert resp.json()["username"] == username
@@ -35,6 +67,33 @@ def test_accounts(client, django_user_model):
     assert user.last_name == user_last_name
     assert user.email == user_email
     assert user.groups.all()[0].name == Roles.CLIENT
+
+    resp = client.post("/accounts/create/", user_data)
+    assert resp.status_code == 406
+    assert resp.json()["error"] == "There is already user with this username."
+
+    resp = client.post("/accounts/create/", user_data_no_password)
+    assert resp.status_code == 406
+    assert resp.json()["error"] == "Your data is incorrect."
+
+    resp = client.post("/accounts/create/", user_data_no_username)
+    assert resp.status_code == 406
+    assert resp.json()["error"] == "Your data is incorrect."
+
+    resp = client.post("/accounts/create/", user_data_no_email)
+    assert resp.status_code == 406
+    assert resp.json()["error"] == "Your data is incorrect."
+
+    resp = client.post("/accounts/create/", user_data_no_last_name)
+    assert resp.status_code == 406
+    assert resp.json()["error"] == "Your data is incorrect."
+
+    resp = client.post("/accounts/create/", user_data_no_first_name)
+    assert resp.status_code == 406
+    assert resp.json()["error"] == "Your data is incorrect."
+
+    users = django_user_model.objects.all()
+    assert len(users) == 1
 
     # TEST GET TOKEN
 
